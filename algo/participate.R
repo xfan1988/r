0e01b07e66fd27fcc28f3649b380c143070fc2b8
Xu Fan <xfan1988@yahoo.com>
2021-09-16 18:33:13 -0400

    Modify participate algo simulation

--- algo/participate.R
+++ algo/participate.R
@@ -1,6 +1,6 @@
 #Simulate: Target 60 minutes, POV, Delta=5 minutes
 #r=0.1
-#N=6000 shares
+#N=6000 shares order size
 
 #Generate Order Sequence (Time, Size)
 
@@ -15,28 +15,28 @@
 #Simulate volume arrival as 1000 shares per minute with a random change
 #(500-1500)
 
-#Project volume as 1000 shares per minute
-
+#Project market volume as 1000 shares per minute
 set.seed(200)
 V_M = cumsum(c(0,100*(5+round(runif(65)*10))))
 
-v_m = 1000          #Projected per-minute volume
-V_P = N * cumsum(c(0, rep(v_m, 60)))/sum(c(0, rep(v_m, 60))) #Depends on Volume Profile
-
 r = 0.1
+
 u_pct = 0.05
 u_sh  = 1000
 
-Delta = 5       #5-minutes
-n = 60
+Delta = 5       #5-minute bin look-forward; target = V_P[i+Delta]
+n = 60          #1-minute bin: re-evaluate;
 N = 100*n
-lmda = 0.4 #0.2
+lmda = 0.4 #0.2   #factor to translate q1 to child order size
+
+v_m = 1000          # Projected per-minute volume
+V_P = N * cumsum(c(0, rep(v_m, 60)))/sum(c(0, rep(v_m, 60))) #Target-schedule (VWAP/TWAP)
 
 genChildOrder = function(strategy="POV", n=10){
   a = l = filled = K = rep(0,n)
   q1 = 0
   for (i in 1:n){
-   v = v_m * Delta
+   v = v_m * Delta      #Market volume in bin
    V = V_M[i]
    if ((strategy=="VWAP") || (strategy=="TWAP"))
    {
@@ -47,13 +47,19 @@ genChildOrder = function(strategy="POV", n=10){
    {
     q1 = ((r*(V+v))-K[i])/(1-r)
    }
-   U_pct = (((r+u_pct)*(V))-K[i])/(1-(r+u_pct))
-   U_sh  = (((r)*(V))-K[i])/(1-r) + u_sh
-   l[i] = min(q1, U_pct, U_sh, N-K[i])
+   
+   # q1: maximum quantity to fill during the bin by strategy definition
+   # r: target participation
+   # K: cumulative filled quantity
+   
+   U_pct = (((r+u_pct)*(V))-K[i])/(1-(r+u_pct)) #percent-upper-band
+   U_sh  = (((r)*(V))-K[i])/(1-r) + U_sh        #share-upper-band
+   
+   l[i] = min(q1, U_pct, U_sh, N-K[i])    #l: distance to band, as maximum child order size
    a[i] = lmda * l[i]
-   filled[i] = 100*round(a[i] * runif(1) * 0.01)
-   K[i] = K[i] + filled[i]
-   rpov = K[i]/(V_M[i+1])
+   filled[i] = 100*round(a[i] * runif(1) * 0.01)   #simulate fill probability during the interval
+   K[i] = K[i] + filled[i]    # Cumulative fill quantity
+   rpov = K[i]/(V_M[i+1])     # cumulative POV
    #cat(paste(i, l[i], a[i], filled[i], K[i], rpov, "\n"))
    K[i+1] = K[i]
   }
@@ -64,22 +70,21 @@ genChildOrder = function(strategy="POV", n=10){
 #Question: How to set re-evaluation trigger for VWAP algo? Always set as 1-minute?
 #How often are child order re-sent? On trigger?
 
-#TODO: Add dark fills
-#Tighter to target (adjust share band)
+# Graph
+# Target
+# Actual
+# With band
 
-#Target
-#Actual
-#With band
-
-o = genChildOrder(strategy="VWAP",n=60)
-plot(o$K, type="l")
+o = genChildOrder(strategy="VWAP", n=60)
+plot(o$K, type="l", xlab="Time")
 lines(V_P, lty=2)
-lines(pmax(0,V_P - u_sh), lty=3)
-lines(pmax(0,V_P + u_sh), lty=3)
+lines(pmax(0, V_P - u_sh), lty=3)
+lines(pmax(0, V_P + u_sh), lty=3)
 #lines(V_P*(1+0.05), lty=4)
 #lines(V_P*(1-0.05), lty=4)
 
-
 plot(cumsum(o$filled)/cumsum(o$a), main="Fill Ratio")
 
-
+#Add:
+# Modify band: Tighter to target (adjust share band)
+# Add dark fills
